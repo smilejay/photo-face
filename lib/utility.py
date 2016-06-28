155c1cc566ca0577c4711c24854ffc0eea4398de
Jay <smile665@gmail.com>
2016-06-28 15:47:50 +0800

    create dir if it doesn't exist

--- lib/utility.py
+++ lib/utility.py
@@ -4,11 +4,16 @@ Created on June 28, 2016
 @summary: some utilities in the system.
 @author: Jay <smile665@gmail.com>
 '''
+import os
+
 security_key = 'hello'
 
 
 def save_file(src_f, dest_f_path):
     ''' save file to a destination file path. '''
+    dest_dir = os.path.dirname(os.path.abspath(dest_f_path))
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
     with open(dest_f_path, 'wb') as f:
         for chunk in src_f.chunks():
             f.write(chunk)
