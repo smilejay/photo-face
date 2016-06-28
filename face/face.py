1ab203322232913d0969b1ddcc42f21285b9e8c1
Jay <smile665@gmail.com>
2016-06-28 19:37:59 +0800

    use cv2 instead of cv; also did refactoring

--- face/face.py
+++ face/face.py
@@ -1,37 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from PIL import Image, ImageDraw
-import cv
+import cv2
 import os
+
+#frontalface_xml = '/tmp/haarcascade_frontalface_default.xml'
+frontalface_xml = '/usr/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'
 
 
 def detect_object(image):
     '''检测图片，获取人脸在图片中的坐标'''
-    grayscale = cv.CreateImage((image.width, image.height), 8, 1)
-    cv.CvtColor(image, grayscale, cv.CV_BGR2GRAY)
-
-    cascade = cv.Load(
-        '/usr/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
-    rect = cv.HaarDetectObjects(
-        grayscale, cascade, cv.CreateMemStorage(),
-        1.1, 2, cv.CV_HAAR_DO_CANNY_PRUNING, (20, 20))
-
-    result = []
-    for r in rect:
-        result.append((r[0][0]-r[0][2]*0.1,
-                       r[0][1]-r[0][3]*0.1,
-                       r[0][0]+r[0][2]*1.1,
-                       r[0][1]+r[0][3]*1.1))
-
-    return result
+    ret = []
+    face_cascade = cv2.CascadeClassifier(frontalface_xml)
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        ret.append((x-w*0.1, y-y*0.4, x+w*1.1, y+h*1.1))
+    return ret
 
 
 def extract_face(infile, outfile):
     '''在原图上截取第1个头像并保存为outfile'''
-    image = cv.LoadImage(infile)
-    if image:
-        faces = detect_object(image)
-
+    faces = detect_object(infile)
     im = Image.open(infile)
     if faces:
         out_dir = os.path.dirname(os.path.abspath(outfile))
@@ -41,7 +32,7 @@ def extract_face(infile, outfile):
         draw.rectangle(faces[0], outline=(255, 0, 0))
         im.crop(faces[0]).save(outfile)
     else:
-        print 'Error: cannot detect faces on %s' % infile
+        return 'Error: cannot detect faces on %s' % infile
 
 
 if __name__ == '__main__':
