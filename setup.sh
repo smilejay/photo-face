bea889500671df40531788e92e61c4068f1bafbf
Jay <smile665@gmail.com>
2016-06-29 09:42:54 +0800

    add python-devel/gunicorn in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # the setup script for CentOS 7.x
-yum install -y gcc cmake
+yum install -y gcc cmake python-devel
 yum install -y zlib zlib-devel libjpeg-turbo libjpeg-turbo-devel freetype freetype-devel
 yum install -y numpy opencv*
 easy_install -i https://pypi.tuna.tsinghua.edu.cn/simple pip
 pip install Pillow -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install Django djangorestframework -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install gunicorn futures trollius -i https://pypi.tuna.tsinghua.edu.cn/simple
